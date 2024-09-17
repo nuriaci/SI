11a306385954cdcb6e347bdcc7372a2aeda4b5aa
nuriaci <nuria.ci@hotmail.es>
2024-09-17 11:54:09 +0200

    rsa, aes encrypt

--- encrypt.py
+++ encrypt.py
@@ -2,10 +2,16 @@ import os
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import (
+    Cipher, algorithms, modes
+)
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
 
 class Encrypt:
 
-    def __init__(self, passwd: bytes):
+    def _init_(self, passwd: bytes):
 
         with open("", "rb") as key_file:    
             pb_key = key_file.read()
@@ -22,9 +28,30 @@ class Encrypt:
                 backend=default_backend()
             )
 
-def encrypt():
-    k = AESGCM.generate_key(bit_length=128)
-    c = rsa_encrypt(pk, k) + aes_encrypt(k, data)
+    def encrypt():
+        k = AESGCM.generate_key(bit_length=128)
+        c = rsa_encrypt(pk, k) + aes_encrypt(k, data)
 
 
 
+    def rsa_encrypt(pk, k) -> bytes:
+        return pk.encrypt(
+        k,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    
+    def aes_encrypt(k,data,as_data):
+        iv = os.urandom(12)
+
+        encryptor = Cipher(
+            algorithms.AES(k),
+            modes.GCM(iv),
+        ).encryptor()
+
+        encryptor.authenticate_additional_data(as_data)
+        ciphertext = encryptor.update(data) + encryptor.finalize()
+        return (iv, ciphertext, encryptor.tag)
